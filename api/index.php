aa6857f74ba794d596efee2f0562c5505547fd47
Grace Kelly <graceksd16@gmail.com>
2026-01-22 17:29:49 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+// Buat folder yang dibutuhkan di /tmp agar tidak error Read-only
+$required_dirs = ['/tmp/storage/framework/views', '/tmp/storage/framework/sessions', '/tmp/storage/framework/cache', '/tmp/bootstrap/cache'];
+foreach ($required_dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
